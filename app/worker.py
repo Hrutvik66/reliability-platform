7ec58ecaeb165b62d8c0b518a6a086f8c20fd0ff
Hrutvik66 <hrutvikmalshikare66@gmail.com>
2026-01-13 23:24:28 +0530

    Fix: Prometheus scraping fix

--- app/worker.py
+++ app/worker.py
@@ -2,8 +2,14 @@ import time
 from app.utils.db_wait import wait_for_db
 from app.services.scheduler import start_scheduler
 
+from prometheus_client import start_http_server
+
 def main():
     wait_for_db()
+    
+    # Start Prometheus metrics server
+    start_http_server(8001)
+    
     start_scheduler()
 
     # Keep container alive
